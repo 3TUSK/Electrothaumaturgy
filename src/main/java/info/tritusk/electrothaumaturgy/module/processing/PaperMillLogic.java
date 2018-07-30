96888a052cbc885e6a691f8ea05e6dc726767a4b
3TUSK <urey.s.knowledge@gmail.com>
2018-07-30 13:08:48 +0800

    Paper Mill refactor
    * Now requires extra Fabrico essentia of 1 unit per paper
    * Now doesn't continuously draining essentia from pipes
    * Proper serialization
    * Client<->Server synchronization

--- src/main/java/info/tritusk/electrothaumaturgy/module/processing/PaperMillLogic.java
+++ src/main/java/info/tritusk/electrothaumaturgy/module/processing/PaperMillLogic.java
@@ -2,9 +2,14 @@ package info.tritusk.electrothaumaturgy.module.processing;
 
 import ic2.api.energy.tile.IEnergyEmitter;
 import ic2.api.energy.tile.IEnergySink;
+import info.tritusk.electrothaumaturgy.internal.Directions;
+import info.tritusk.electrothaumaturgy.internal.ElectricDeviceLogicBase;
 import info.tritusk.electrothaumaturgy.internal.SingleSlotItemHandler;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
@@ -17,11 +22,11 @@ import thaumcraft.api.aspects.IEssentiaTransport;
 
 import javax.annotation.Nullable;
 
-public class PaperMillLogic extends TileEntity implements ITickable, IEnergySink, IAspectContainer, IEssentiaTransport {
+public final class PaperMillLogic extends ElectricDeviceLogicBase implements ITickable, IEnergySink, IAspectContainer, IEssentiaTransport {
 
-    private static final int MAX_ENERGY = 8000, MAX_ESSENTIA = 500;
+    private static final int MAX_ENERGY = 8000, MAX_ESSENTIA = 200, MAX_CATALYST = 200;
 
-    private int energy, essentia;
+    private int energy, essentia, catalyst; // essentia is Herba, catalyst is Fabrico
     private SingleSlotItemHandler inventory = new SingleSlotItemHandler();
 
     @Override
@@ -30,8 +35,9 @@ public class PaperMillLogic extends TileEntity implements ITickable, IEnergySink
             return;
         }
 
-        if (essentia >= 8 && energy >= 200) {
+        if (essentia >= 8 && catalyst > 0 && energy >= 200) {
             essentia -= 8;
+            catalyst--;
             energy -= 200;
             ItemStack currentContent = inventory.getRawContent();
             if (currentContent.isEmpty()) {
@@ -41,6 +47,88 @@ public class PaperMillLogic extends TileEntity implements ITickable, IEnergySink
             }
             inventory.setRawContent(currentContent);
         }
+
+        boolean requireUpdate = false;
+
+        for (EnumFacing direction : EnumFacing.HORIZONTALS) {
+            if (essentia >= MAX_ESSENTIA) {
+                break;
+            }
+            TileEntity tile = this.getWorld().getTileEntity(this.getPos().offset(direction));
+            if (tile instanceof IEssentiaTransport) {
+                IEssentiaTransport channel = (IEssentiaTransport) tile;
+                EnumFacing opposite = direction.getOpposite();
+                if (channel.canOutputTo(opposite)
+                        && channel.getSuctionType(opposite) == Aspect.PLANT
+                        && channel.getSuctionAmount(opposite) < this.getSuctionAmount(direction)
+                        && this.getSuctionAmount(direction) >= channel.getMinimumSuction()) {
+                    this.addToContainer(Aspect.PLANT, channel.takeEssentia(Aspect.PLANT, 1, opposite));
+                    requireUpdate = true;
+                    if (essentia >= MAX_ESSENTIA) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (EnumFacing direction : Directions.VERTICALS) {
+            if (catalyst >= MAX_CATALYST) {
+                break;
+            }
+            TileEntity tile = this.getWorld().getTileEntity(this.getPos().offset(direction));
+            if (tile instanceof IEssentiaTransport) {
+                IEssentiaTransport channel = (IEssentiaTransport) tile;
+                EnumFacing opposite = direction.getOpposite();
+                if (channel.canOutputTo(opposite)
+                        && channel.getSuctionType(opposite) == Aspect.CRAFT
+                        && channel.getSuctionAmount(opposite) < this.getSuctionAmount(direction)
+                        && this.getSuctionAmount(direction) >= channel.getMinimumSuction()) {
+                    this.addToContainer(Aspect.CRAFT, channel.takeEssentia(Aspect.CRAFT, 1, opposite));
+                    requireUpdate = true;
+                    if (catalyst >= MAX_CATALYST) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (requireUpdate) {
+            this.getWorld().notifyBlockUpdate(pos, world.getBlockState(pos), world.getBlockState(pos), 2);
+        }
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound data) {
+        data.setInteger("energy", energy);
+        data.setInteger("essentia", essentia);
+        data.setInteger("catalyst", catalyst);
+        return super.writeToNBT(data);
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound data) {
+        super.readFromNBT(data);
+        this.energy = data.getInteger("energy");
+        this.essentia = data.getInteger("essentia");
+        this.catalyst = data.getInteger("catalyst");
+    }
+
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        NBTTagCompound data = new NBTTagCompound();
+        data.setInteger("energy", energy);
+        data.setInteger("essentia", essentia);
+        data.setInteger("catalyst", catalyst);
+        return new SPacketUpdateTileEntity(this.getPos(), 1, data);
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
+        super.onDataPacket(net, packet);
+        NBTTagCompound data = packet.getNbtCompound();
+        this.energy = data.getInteger("energy");
+        this.essentia = data.getInteger("essentia");
+        this.catalyst = data.getInteger("catalyst");
     }
 
     @Override
@@ -81,7 +169,14 @@ public class PaperMillLogic extends TileEntity implements ITickable, IEnergySink
 
     @Override
     public AspectList getAspects() {
-        return this.essentia == 0 ? null : new AspectList().add(Aspect.PLANT, essentia);
+        AspectList list = new AspectList();
+        if (essentia > 0) {
+            list.add(Aspect.PLANT, essentia);
+        }
+        if (catalyst > 0) {
+            list.add(Aspect.CRAFT, catalyst);
+        }
+        return list;
     }
 
     @Override
@@ -91,21 +186,28 @@ public class PaperMillLogic extends TileEntity implements ITickable, IEnergySink
 
     @Override
     public boolean doesContainerAccept(Aspect aspect) {
-        return aspect == Aspect.PLANT;
+        return aspect == Aspect.PLANT || aspect == Aspect.CRAFT;
     }
 
     @Override
     public int addToContainer(Aspect aspect, int increment) {
-        if (aspect != Aspect.PLANT) {
+        if (aspect == Aspect.PLANT) {
+            this.essentia += increment;
+            int diff = Math.max(0, essentia - MAX_ESSENTIA);
+            if (diff > 0) {
+                this.essentia = MAX_ESSENTIA;
+            }
+            return diff > 0 ? diff : 0;
+        } else if (aspect == Aspect.CRAFT) {
+            this.catalyst += increment;
+            int diff = Math.max(0, catalyst - MAX_CATALYST);
+            if (diff > 0) {
+                this.catalyst = MAX_CATALYST;
+            }
+            return diff > 0 ? diff : 0;
+        } else {
             return increment;
         }
-
-        this.essentia += increment;
-        int diff = Math.max(0, essentia - MAX_ESSENTIA);
-        if (diff > 0) {
-            this.essentia = MAX_ESSENTIA;
-        }
-        return diff > 0 ? diff : 0;
     }
 
     @Override
@@ -116,12 +218,18 @@ public class PaperMillLogic extends TileEntity implements ITickable, IEnergySink
 
     @Override
     public boolean doesContainerContainAmount(Aspect aspect, int amount) {
-        return aspect == Aspect.PLANT && this.essentia >= amount;
+        if (aspect == Aspect.PLANT) {
+            return essentia >= amount;
+        } else if (aspect == Aspect.CRAFT) {
+            return catalyst >= amount;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public int containerContains(Aspect aspect) {
-        return aspect == Aspect.PLANT ? this.essentia : 0;
+        return aspect == Aspect.PLANT ? essentia : aspect == Aspect.CRAFT ? catalyst : 0;
     }
 
     @Deprecated
@@ -160,7 +268,7 @@ public class PaperMillLogic extends TileEntity implements ITickable, IEnergySink
 
     @Override
     public Aspect getSuctionType(EnumFacing facing) {
-        return Aspect.PLANT;
+        return facing.getAxis() == EnumFacing.Axis.Y ? Aspect.CRAFT : Aspect.PLANT;
     }
 
     @Override
@@ -176,17 +284,17 @@ public class PaperMillLogic extends TileEntity implements ITickable, IEnergySink
 
     @Override
     public int addEssentia(Aspect aspect, int amount, EnumFacing facing) {
-        return amount - this.addToContainer(aspect, amount);
+        return aspect == Aspect.PLANT || aspect == Aspect.CRAFT ? amount - this.addToContainer(aspect, amount) : 0;
     }
 
     @Override
     public Aspect getEssentiaType(EnumFacing facing) {
-        return this.essentia > 0 ? Aspect.PLANT : null;
+        return facing.getAxis() == EnumFacing.Axis.Y ? Aspect.CRAFT : Aspect.PLANT;
     }
 
     @Override
     public int getEssentiaAmount(EnumFacing facing) {
-        return this.essentia;
+        return facing.getAxis() == EnumFacing.Axis.Y ? catalyst : essentia;
     }
 
     @Override
