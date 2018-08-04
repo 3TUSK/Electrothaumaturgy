3f09fce05b4a6b1016c40308ad06f77f24de2897
3TUSK <urey.s.knowledge@gmail.com>
2018-08-04 21:38:08 +0800

    Begin of research lore writing. Just a placeholder right now...

--- src/main/java/info/tritusk/electrothaumaturgy/ElectroThaumaturgy.java
+++ src/main/java/info/tritusk/electrothaumaturgy/ElectroThaumaturgy.java
@@ -3,7 +3,13 @@ package info.tritusk.electrothaumaturgy;
 import ic2.api.item.ElectricItem;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import thaumcraft.api.ThaumcraftApi;
+import thaumcraft.api.aspects.Aspect;
+import thaumcraft.api.aspects.AspectList;
+import thaumcraft.api.research.ResearchCategories;
 
 @Mod(
         modid = ElectroThaumaturgy.MOD_ID,
@@ -25,4 +31,16 @@ public final class ElectroThaumaturgy {
             return icon;
         }
     };
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        ThaumcraftApi.registerResearchLocation(new ResourceLocation(MOD_ID, "research/electrothaumaturgy.json"));
+        ResearchCategories.registerCategory(
+                "ELECTROTHAUMATURGY",
+                "ET_ORIGIN", // It must be a research defined in your research json data.
+                new AspectList().add(Aspect.ENERGY, 10),
+                new ResourceLocation(MOD_ID, "textures/items/drill_of_core.png"),
+                new ResourceLocation("thaumcraft", "textures/gui/gui_research_back_4.jpg")
+        );
+    }
 }
